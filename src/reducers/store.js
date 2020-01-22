ff77b01acff87fb50f79381d1af680030c8dd6eb
Mark Simpkins <msimpkins@gmail.com>
2020-01-22 15:22:23 +0000

    updated redux call

--- src/reducers/store.js
+++ src/reducers/store.js
@@ -1,11 +1,11 @@
-import {createStore, applyMiddleware} from 'redux';
-
+import { createStore, applyMiddleware } from 'redux';
 import reducers from '../reducers'
-
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(reducers,
-	{'login':'', 'register':''},
-	applyMiddleware(),
-	window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
-
+	{ 'profile': '' },
+	composeEnhancer(applyMiddleware(thunk)),
+	// applyMiddleware(),
+	// window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+)
 export default store
 
