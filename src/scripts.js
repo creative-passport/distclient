03c223f4024210b511aa42730896ffe28a15171f
Mark Simpkins <msimpkins@gmail.com>
2020-05-14 13:44:42 +0100

    set dev to false in scripts.js

--- src/scripts.js
+++ src/scripts.js
@@ -6,7 +6,7 @@ const proxyurl = "https://cors-anywhere.herokuapp.com/"
 const urldev = proxyurl + 'https://vd5e0pnn7i.execute-api.eu-west-2.amazonaws.com/dev/'
 const urlprod = 'https://vd5e0pnn7i.execute-api.eu-west-2.amazonaws.com/prod/'
 
-let dev = true
+let dev = false
 let url
 if (dev) {
     url = urldev
